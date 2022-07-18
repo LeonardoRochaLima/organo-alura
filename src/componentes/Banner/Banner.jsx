bb982e2148a6bf1b1296798ed275a01ac9508c75
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-07-18 16:26:19 -0300

    Primeiro commit + Adição de um componente

---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+function Banner() {
+  //JSX
+  return (
+    <header className='banner'>
+      <img
+        src="imagens/banner.png"
+        alt="O banner principal da página do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
