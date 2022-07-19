8a8ca1a52287920b46b4c666e636aeffb8d3eee6
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-07-19 16:32:46 -0300

    Criando um componente de select/dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem"/>
+      <Formulario/>
     </div>
   );
 }
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,28 @@
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
+
+const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu Nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
