98b3c5da2dd0a495346fd44cbeb92d5e653fad98
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-07-20 13:52:43 -0300

    Melhorando os estilos do Rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,21 +1,19 @@
-import './Rodape.css'
+import "./Rodape.css";
 
-const Rodape = () =>{
-    return (
-        <footer>
-            <section className='rodape'>
-                <div className='logos-redes-sociais'>
-                    <img src='imagens/fb.png' alt='Logo Facebook'/>
-                    <img src='imagens/tw.png' alt='Logo Twitter'/>
-                    <img src='imagens/ig.png' alt='Logo Instagram'/>
-                </div>
-                <img src='imagens/logo.png' alt='Logo Rodapé'/>
-                <p>
-                    Desenvolvido por Leo
-                </p>
-            </section> 
-        </footer>
-    )
-}
+const Rodape = () => {
+  return (
+    <footer>
+      <section className="rodape">
+        <div className="logos-redes-sociais">
+          <img src="imagens/fb.png" alt="Logo Facebook" />
+          <img src="imagens/tw.png" alt="Logo Twitter" />
+          <img src="imagens/ig.png" alt="Logo Instagram" />
+        </div>
+        <img src="imagens/logo.png" alt="Logo Rodapé" />
+        <p>Desenvolvido por Leo</p>
+      </section>
+    </footer>
+  );
+};
 
-export default Rodape
+export default Rodape;
