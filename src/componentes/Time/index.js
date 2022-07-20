6f9801f2957bba4be909a071afae2d91784d0618
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-07-20 12:04:15 -0300

    Escondendo os times que estão vazios usando uma diretiva de condicional do JSX

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,6 +3,7 @@ import "./Time.css";
 
 const Time = (props) => {
   return (
+    props.colaboradores.length > 0 &&
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
