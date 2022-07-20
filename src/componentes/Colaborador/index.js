e72421eb33db3ebbdf4ed18e5bd72aed3ec0a2f7
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-07-20 11:00:41 -0300

    Criando o dumb component com os cards do colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/LeonardoRochaLima.png' alt='Icone do Colaborador'/>
+            </div>
+            <div className='rodape'>
+                <h4>Leonardo Lima</h4>
+                <h5>Programador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,12 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
   return (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <Colaborador/>
+      <Colaborador/>
     </section>
   );
 };
